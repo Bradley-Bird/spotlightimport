caf1970f031f2d5c5b555884b84177eb3ce3ecaa
Bradley-Bird <Bradleybird2@icloud.com>
2022-02-01 11:27:56 -0800

    added demo,made nested array robots, created test for renderRobots.

---
+++ robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+    {
+        name: 'Bender',
+        info: {
+            appearsIn: 'Futurama',
+            quote: '"O cruel fate, to be thusly boned! Ask not for whom the bone bones—it bones for thee.”',
+            funFact: 'Bender would like to kill all humans',
+        },
+    },
+    {
+        name: 'RoboCop',
+        info: {
+            appearsIn: 'RoboCop',
+            quote: '"My friends call me Murphy. You call me...RoboCop"',
+            funFact: 'RoboCop wants to protect most humans',
+        },
+    },
+    {
+        name: 'Iron Giant',
+        info: {
+            appearsIn: 'The Iron Giant',
+            quote: 'You stay. I go. No following.',
+            funFact: 'Vin Diesel is the voice of The Iron Giant',
+        },
+    },
+];
---
+++ test/utils.test.js
@@ -0,0 +1,49 @@
+// IMPORT MODULES under test here:
+// import { example } from '../example.js';
+
+import { robots } from '../robots.js';
+import { renderAnimal, renderRobots } from '../utils.js';
+
+const test = QUnit.test;
+
+test('renderAnimal should return a <div> with dog info', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = `<div class="animal"><h2>DOG</h2><img src="./assets/icons8-dog-100.png"><p>Mammal with 4 legs and a cuteness rating of 10.</p></div>`;
+
+    //Act
+    // Call the function you're testing and set the result to a const
+    const actual = renderAnimal({
+        type: 'dog',
+        numberOfLegs: 4,
+        class: 'Mammal',
+        cuteness: 10,
+        image: 'icons8-dog-100.png',
+    });
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual.outerHTML, expected);
+});
+
+
+test('should return list with nested array lists', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = `<div class="robots" id="robotlist">
+    <h2>BENDER</h2>
+    <ul>
+        <li>Futurama</li>
+        <li>"O cruel fate, to be thusly boned! Ask not for whom the bone bones—it bones for thee.”</li>
+        <li>Bender would like to kill all humans</li>
+    </ul>
+</div>`;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = renderRobots(robots);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
